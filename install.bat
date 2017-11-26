16bc4dd690e6fcc3ce5ca745aacbd5faaeba0787
tim-xs <30874117+tim-xs@users.noreply.github.com>
2017-11-26 11:50:23 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,4 @@
+py -m pip install pandas
+py -m pip install pandas-datareader
+py -m pip install beautifulsoup4 
+py -m pip install matplotlib
---
+++ t.py
@@ -0,0 +1,6 @@
+import os 
+
+a = os.path.getsize("stock_data/BF.B.csv") 
+b = os.path.getsize("stock_data/AAPL.csv") 
+
+print(a, b)
